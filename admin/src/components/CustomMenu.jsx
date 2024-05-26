90926b30892de0dd69fe17f34e906928c0661753
MawinS <meow7747@gmail.com>
2024-05-26 15:04:37 +0700

    fix: manage file

--- admin/src/components/CustomMenu.jsx
+++ admin/src/components/CustomMenu.jsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
 import { Menu, MenuItemLink, usePermissions } from 'react-admin';
-import ListIcon from '@mui/icons-material/List';
 import PeopleIcon from '@mui/icons-material/People';
-import SettingsIcon from '@mui/icons-material/Settings';
+import VpnKeyIcon from '@mui/icons-material/VpnKey';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FlagIcon from '@mui/icons-material/Flag';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import JoinInnerIcon from '@mui/icons-material/JoinInner';
+import ForumIcon from '@mui/icons-material/Forum';
+import PauseCircleFilledIcon from '@mui/icons-material/PauseCircleFilled';
+import BlockIcon from '@mui/icons-material/Block';
 
 const CustomMenu = () => {
     const { permissions } = usePermissions();
@@ -11,15 +17,50 @@ const CustomMenu = () => {
         <Menu>
             <MenuItemLink
                 to="/userAuth"
-                primaryText="Users"
+                primaryText="Auth"
+                leftIcon={<VpnKeyIcon />}
+            />
+            <MenuItemLink
+                to="/userAuth"
+                primaryText="Infoes"
                 leftIcon={<PeopleIcon />}
             />
             <MenuItemLink
                 to="/preferences"
                 primaryText="Preferences"
-                leftIcon={<SettingsIcon />}
+                leftIcon={<FavoriteIcon />}
             />
-            {/* Add more menu items as needed */}
+            <MenuItemLink
+                to="/preferences"
+                primaryText="Likes"
+                leftIcon={<ThumbUpIcon />}
+            />
+            <MenuItemLink
+                to="/userAuth"
+                primaryText="Matches"
+                leftIcon={<JoinInnerIcon />}
+            />
+            <MenuItemLink
+                to="/userAuth"
+                primaryText="Messages"
+                leftIcon={<ForumIcon />}
+            />
+            <MenuItemLink
+                to="/userAuth"
+                primaryText="Chat Status"
+                leftIcon={<PauseCircleFilledIcon />}
+            />
+            <MenuItemLink
+                to="/userAuth"
+                primaryText="Blocks"
+                leftIcon={<BlockIcon />}
+            />
+            <MenuItemLink
+                to="/preferences"
+                primaryText="Reports"
+                leftIcon={<FlagIcon />}
+            />
+
         </Menu>
     );
 };
